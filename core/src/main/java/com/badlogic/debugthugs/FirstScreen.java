7a218ac8f3c516996683433eebc36fcd783ea360
rosie-mae <234466771+rosie-mae@users.noreply.github.com>
2025-10-22 01:01:59 +0100

    finished simple libGDX tutorial game

--- core/src/main/java/com/badlogic/debugthugs/FirstScreen.java
+++ core/src/main/java/com/badlogic/debugthugs/FirstScreen.java
@@ -9,22 +9,32 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+    float dropTimer;
     Texture backgroundTexture;
     Texture bucketTexture;
     Sprite bucketSprite;
     Texture dropTexture;
     Sound dropSound;
     Music music;
+    Array<Sprite> dropSprites;
     SpriteBatch spriteBatch;
     FitViewport viewport;
+    Rectangle bucketRectangle;
+    Rectangle dropRectangle;
+
     @Override
     public void show() {
         // Prepare your screen here.
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
         backgroundTexture = new Texture("background.png");
         bucketTexture = new Texture("bucket.png");
         bucketSprite = new Sprite(bucketTexture);
@@ -32,8 +42,12 @@ public class FirstScreen implements Screen {
         dropTexture = new Texture("drop.png");
         dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
         music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
-        spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
+        dropSprites = new Array<>();
+        bucketRectangle = new Rectangle();
+        dropRectangle = new Rectangle();
+        music.setLooping(true);
+        music.setVolume(0.5f);
+        music.play();
     }
 
     @Override
@@ -56,6 +70,34 @@ public class FirstScreen implements Screen {
     }
 
     private void logic() {
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+        float bucketWidth = bucketSprite.getWidth();
+        float bucketHeight = bucketSprite.getHeight();
+
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
+
+        float delta = Gdx.graphics.getDeltaTime();
+        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
+        for (int i = dropSprites.size - 1; i >= 0; i--) {
+            Sprite dropSprite = dropSprites.get(i); // Get the sprite from the list
+            float dropWidth = dropSprite.getWidth();
+            float dropHeight = dropSprite.getHeight();
+
+            dropSprite.translateY(-2f * delta);
+            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
+            // if the top of the drop goes below the bottom of the view, remove it
+            if (dropSprite.getY() < -dropHeight) dropSprites.removeIndex(i);
+            else if (bucketRectangle.overlaps(dropRectangle)) {
+                dropSprites.removeIndex(i);
+                dropSound.play();
+            }
+        }
+        dropTimer += delta;
+        if (dropTimer > 1f) {
+            dropTimer = 0;
+            createDroplet();
+        }
 
     }
 
@@ -69,7 +111,24 @@ public class FirstScreen implements Screen {
         spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
         bucketSprite.draw(spriteBatch);
 
+        for(Sprite dropSprite : dropSprites) {
+            dropSprite.draw(spriteBatch);
+        }
+
         spriteBatch.end();
+    }
+
+    private void createDroplet() {
+        float dropWidth = 1;
+        float dropHeight = 1;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        Sprite dropSprite = new Sprite(dropTexture);
+        dropSprite.setSize(dropWidth, dropHeight);
+        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
+        dropSprite.setY(worldHeight);
+        dropSprites.add(dropSprite);
     }
     @Override
     public void resize(int width, int height) {
