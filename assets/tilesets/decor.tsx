597619daf665f2957c25f1549b03126b3c31690e
rosie-mae <234466771+rosie-mae@users.noreply.github.com>
2025-10-28 20:32:23 +0000

    Made the player start in the correct room and added collisions

--- assets/tilesets/decor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="decor" tilewidth="32" tileheight="32" tilecount="0" columns="24">
- <image source="Inside_C.png" width="768" height="768"/>
-</tileset>
--- core/src/main/java/com/badlogic/debugthugs/FirstScreen.java
+++ core/src/main/java/com/badlogic/debugthugs/FirstScreen.java
@@ -11,12 +11,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -41,8 +41,9 @@ public class FirstScreen implements Screen {
     int mins;
     int seconds;
     OrthographicCamera camera;
-    TiledMap map;
+    TiledMap map = new TmxMapLoader().load("maps/maze_map.tmx");
     OrthogonalTiledMapRenderer renderer;
+    private TiledMapTileLayer collisionLayer;
 
     public FirstScreen(Game game) {
         this.game = game;
@@ -51,27 +52,28 @@ public class FirstScreen implements Screen {
     @Override
     public void show() {
         viewport = new FitViewport(800, 600);
+        //made a separate camera rather than using the viewport so that the timer stays in the top corner
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 600);
-        map = new TmxMapLoader().load("maps/maze_map.tmx");
+        camera.zoom = 0.6f;
+        MapLayer wallsLayer = map.getLayers().get("Walls");
+        collisionLayer = (TiledMapTileLayer) wallsLayer;
         renderer = new OrthogonalTiledMapRenderer(map);
+
         bucketTexture = new Texture("bucket.png");
         bucketSprite = new Sprite(bucketTexture);
-        bucketSprite.setSize(32,32);
-        bucketSprite.setPosition(64, 64);
-        camera.position.set(
-            bucketSprite.getX() + bucketSprite.getWidth() / 2f,
-            bucketSprite.getY() + bucketSprite.getHeight() / 2f,
-            0
-        );
-        camera.update();
-        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        bucketSprite.setSize(30,30);
+        bucketSprite.setPosition(710, 1730);
+
         dropSprites = new Array<>();
         bucketRectangle = new Rectangle();
         dropRectangle = new Rectangle();
+        //music stuff
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
         music.setLooping(true);
         music.setVolume(0.5f);
         music.play();
+        //timer stuff
         timeBatch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.WHITE);
@@ -81,6 +83,7 @@ public class FirstScreen implements Screen {
     public void render(float delta) {
         input();
         logic();
+        //sets the camera to position the sprite in the middle of the screen
         camera.position.set(
             bucketSprite.getX() + bucketSprite.getWidth() / 2f,
             bucketSprite.getY() + bucketSprite.getHeight() / 2f,
@@ -97,6 +100,7 @@ public class FirstScreen implements Screen {
         bucketSprite.draw(renderer.getBatch());
         renderer.getBatch().end();
 
+        //timer stuff
         timePassed -= delta;
         if(timePassed <= 0){
             game.setScreen(new LoseScreen(game));
@@ -114,20 +118,70 @@ public class FirstScreen implements Screen {
     private void input() {
         float speed = 128f;
         float delta = Gdx.graphics.getDeltaTime();
+        float moveAmount = speed * delta;
 
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            bucketSprite.translateX(speed * delta);
+            bucketSprite.translateX(moveAmount);
+            if (collision()) {
+                bucketSprite.translateX(-moveAmount);
+            }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            bucketSprite.translateX(-speed * delta);
+            bucketSprite.translateX(-moveAmount);
+            if (collision()) {
+                bucketSprite.translateX(moveAmount);
+            }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            bucketSprite.translateY(speed * delta);
+            bucketSprite.translateY(moveAmount);
+            if (collision()) {
+                bucketSprite.translateY(-moveAmount);
+            }
         }
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            bucketSprite.translateY(-speed * delta);
+            bucketSprite.translateY(-moveAmount);
+            if (collision()) {
+                bucketSprite.translateY(moveAmount);
+            }
         }
 
+    }
+
+    private boolean collision() {
+        float x = bucketSprite.getX();
+        float y = bucketSprite.getY();
+        float width = bucketSprite.getWidth();
+        float height = bucketSprite.getHeight();
+
+        //checks 3 corners of the sprite to see if it's colliding with a wall.
+        //I originally checked 4 corners but it made movement around corners slightly smoother if I checked 3
+        //The last check allows for the player to be walking in front of the wall we're facing slightly
+        if (check_wall(x, y)) {
+            return true;
+        }
+        if (check_wall(x + (width), y)) {
+            return true;
+        }
+        if (check_wall(x + width, y + (height/2))) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean check_wall(float x, float y) {
+        //Figures out the x and y coordinate of the tile the sprite is on
+        int tileX = (int) (x / 32);
+        int tileY = (int) (y / 32);
+
+        //checks the tile at position (x,y)
+        //if there is no tile there, return null
+        //its checking the wall layer of the map, meaning if there isnt a tile in that location theres nothing to collide with
+        TiledMapTileLayer.Cell cell = collisionLayer.getCell(tileX, tileY);
+        if (cell == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     private void logic() {
@@ -139,7 +193,7 @@ public class FirstScreen implements Screen {
     }
 
     private void draw() {
-        ScreenUtils.clear(Color.BLACK);
+        ScreenUtils.clear(220/255f, 157/255f, 126/255f, 1);
         viewport.apply();
     }
 
