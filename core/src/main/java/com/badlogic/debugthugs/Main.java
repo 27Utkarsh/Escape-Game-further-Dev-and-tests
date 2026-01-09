d61bf747fbe274eaca25178ac63abcad02ab6eae
alistudentgit <dfn510@york.ac.uk>
2026-01-09 05:48:24 +0300

    Update Main.java

--- core/src/main/java/com/badlogic/debugthugs/Main.java
+++ core/src/main/java/com/badlogic/debugthugs/Main.java
@@ -6,15 +6,14 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
+ */
 public class Main extends Game {
-
     public SpriteBatch batch;
     public BitmapFont font;
-
     public OrthographicCamera worldCamera;
     public FitViewport worldViewport;
-
     public OrthographicCamera uiCamera;
     public FitViewport uiViewport;
 
@@ -29,12 +28,12 @@ public class Main extends Game {
         uiViewport = new FitViewport(1280, 720, uiCamera);
 
         font = new BitmapFont();
-
         setScreen(new MenuScreen(this));
     }
 
     /**
      * Updates the viewport and stage sizes when the screen changes size.
+     *
      * @param width  new width of the screen
      * @param height new height of the screen
      */
@@ -45,12 +44,8 @@ public class Main extends Game {
     }
 
     @Override
-    public void dispose()
-    {
+    public void dispose() {
         batch.dispose();
         font.dispose();
     }
-
 }
-
-
