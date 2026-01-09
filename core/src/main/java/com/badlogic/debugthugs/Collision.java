e79d449e4414ddf8bfbf23f1c616bf6446d6cac2
alistudentgit <dfn510@york.ac.uk>
2026-01-09 05:33:47 +0300

    Update Collision.java

--- core/src/main/java/com/badlogic/debugthugs/Collision.java
+++ core/src/main/java/com/badlogic/debugthugs/Collision.java
@@ -6,21 +6,20 @@ public class Collision {
 
     /**
      * Checks whether the player is colliding with a wall tile.
+     *
      * @param player The player whose collision boundaries are being checked
      * @return true if the player collides with a wall tile at any of the checked points, false otherwise
      */
     public static boolean collisionCheck(Player player) {
-        //checks 3 corners of the sprite to see if it's colliding with a wall.
-        //I originally checked 4 corners but it made movement around corners slightly smoother if I checked 3
-        //The last check allows for the player to be walking in front of the wall we're facing slightly
+        // checks 3 corners of the sprite to see if its colliding with a wall.
+        // I originally checked 4 corners but it made movement around corners slightly smoother if I checked 3
+        // The last check allows for the player to be walking in front of the wall were facing slightly
         if (checkWall(player, player.playerX, player.playerY)) {
             return true;
         }
-
         if (checkWall(player, player.playerX + player.playerWidth, player.playerY)) {
             return true;
         }
-
         if (checkWall(player, player.playerX, player.playerY + player.playerHeight)) {
             return true;
         }
@@ -31,23 +30,23 @@ public class Collision {
     }
 
     /**
-     * Checks whether there is a wall tile in the wallLayer at the player's coordinates.
+     * Checks whether there is a wall tile in the wallLayer at the players coordinates.
      * Using the fact that the tiles are 32x32 pixels.
      * if this function returns true, it indicates that there is a wall tile.
      *
      * @param player Player whose wallLayer is referenced
-     * @param x The X-Coordinate thats being checked for a wall tile
-     * @param y The Y-Coordinate thats being checked for a wall tile
+     * @param x      The X-Coordinate thats being checked for a wall tile
+     * @param y      The Y-Coordinate thats being checked for a wall tile
      * @return true if the tile at the given coordinates contains a wall tile, false otherwise
      */
     private static boolean checkWall(Player player, float x, float y) {
-        //Figures out the x and y coordinate of the tile the sprite is on
+        // Figures out the x and y coordinate of the tile the sprite is on
         int tileX = (int) (x / 32);
         int tileY = (int) (y / 32);
 
-        //checks the tile at position (x,y)
-        //if there is no tile there, return null
+        // checks the tile at position x,y if there is no tile there, return null
         TiledMapTileLayer.Cell cell = player.wallLayer.getCell(tileX, tileY);
+
         if (cell == null) {
             return false;
         } else {
@@ -62,19 +61,17 @@ public class Collision {
      * @return true if the player collides with a door tile at any of the checked points, false otherwise
      */
     public static boolean door(Player player) {
-        Player.doorInfront = false;
+        player.doorInfront = false;
         if (checkDoor(player, player.playerX + 10, player.playerY)) {
-            Player.doorInfront = true;
+            player.doorInfront = true;
             return true;
         }
-
         if (checkDoor(player, player.playerX + player.playerWidth, player.playerY)) {
-            Player.doorInfront = true;
+            player.doorInfront = true;
             return true;
         }
-
-        if (checkDoor(player, player.playerX + player.playerWidth, player.playerY + (player.playerHeight / 2))) {
-            Player.doorInfront = true;
+        if (checkDoor(player, player.playerX + player.playerWidth, player.playerY + player.playerHeight / 2)) {
+            player.doorInfront = true;
             return true;
         }
 
@@ -82,27 +79,28 @@ public class Collision {
     }
 
     /**
-     * Checks whether there is a door tile in the doorLayer at the player's coordinates.
+     * Checks whether there is a door tile in the doorLayer at the players coordinates.
      * if this function returns true, it indicates that there is a door tile.
      * if the player has previously opened the door, the door will disappear.
+     *
      * @param player Player whose doorLayer is referenced
-     * @param x The X-Coordinate thats being checked for a door tile
-     * @param y The Y-Coordinate thats being checked for a door tile
+     * @param x      The X-Coordinate thats being checked for a door tile
+     * @param y      The Y-Coordinate thats being checked for a door tile
      * @return true if the tile at the given coordinates contains a door tile, false otherwise
      */
     private static boolean checkDoor(Player player, float x, float y) {
         int tileX = (int) (x / 32);
         int tileY = (int) (y / 32);
 
-        // Look up the tile at (tileX, tileY) in the door layer of the map
-        // getCell() returns a tile cell if one exists here, otherwise it returns null
+        // Look up the tile at tileX, tileY in the door layer of the map
+        // getCell returns a tile cell if one exists here, otherwise it returns null
         TiledMapTileLayer.Cell cell = player.doorLayer.getCell(tileX, tileY);
 
-        //if there is no door tile, return false
+        // if there is no door tile, return false
         if (cell == null) {
             return false;
         } else {
-            //if player opens the door, remove the door tile at those coordinates
+            // if player opens the door, remove the door tile at those coordinates
             if (player.open) {
                 player.doorLayer.setCell(tileX, tileY, null);
                 return false;
