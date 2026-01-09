44c7fb696dfc02120f0eaefcc8e7dc0dbd6f8948
alistudentgit <dfn510@york.ac.uk>
2026-01-09 06:01:21 +0300

    Update DuckTest.java

--- core/src/test/java/com/badlogic/debugthugs/DuckTest.java
+++ core/src/test/java/com/badlogic/debugthugs/DuckTest.java
@@ -1,23 +1,30 @@
 package com.badlogic.debugthugs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DuckTest {
+public class DuckTest {
 
+    /**
+     * Initialize the headless backend for testing.
+     */
     @BeforeAll
     public static void init() {
         if (Gdx.files == null) {
             HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-            new HeadlessApplication(new com.badlogic.gdx.ApplicationAdapter() {}, config);
+            new HeadlessApplication(new com.badlogic.gdx.ApplicationAdapter() {
+            }, config);
         }
     }
 
-    //Test if duck is initialized with correct position and size
+    /**
+     * Test if duck is initialized with correct position and size.
+     */
     @Test
     void duckInitialization() {
         Duck duck = new Duck();
@@ -27,13 +34,14 @@ class DuckTest {
         assertEquals(55f, duck.bounds.height, 0.0001f);
     }
 
-    //Test if encountering duck increments bad event counter by one
-    //Test if encountering duck resets player's position
+    /**
+     * Test if encountering duck increments bad event counter by one
+     * and resets players position.
+     */
     @Test
     void encounteringDuck() {
         Duck duck = new Duck();
         duck.bounds.setPosition(680f, 520f);
-        
         Player dummyPlayer = new Player(680f, 520f, null, null);
         dummyPlayer.playerWidth = 50f;
         dummyPlayer.playerHeight = 55f;
