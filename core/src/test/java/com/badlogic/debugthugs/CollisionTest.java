3ccdc5e625c8d4c7c95a161aaaad322e65877d1b
alistudentgit <dfn510@york.ac.uk>
2026-01-09 05:58:52 +0300

    Update CollisionTest.java

--- core/src/test/java/com/badlogic/debugthugs/CollisionTest.java
+++ core/src/test/java/com/badlogic/debugthugs/CollisionTest.java
@@ -3,22 +3,22 @@ package com.badlogic.debugthugs;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
 /**
  * Tests the collision system.
  * Checks the collisions work correctly for all directions in edge cases.
  */
 public class CollisionTest {
-
     private final float playerWidth = 16;
     private final float playerHeight = 16;
 
     @BeforeEach
     void setUp() {
-        // Ensure static variables (in Player) are reset to their initial state.
+        // Ensure static variables in Player are reset to their initial state.
         Player.doorInfront = false;
         Player.open = false;
     }
@@ -28,6 +28,7 @@ public class CollisionTest {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         Player player = new Player(32, 32, wallLayer, doorLayer);
+
         assertFalse(Collision.collisionCheck(player));
     }
 
@@ -37,6 +38,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(1, 1, new TiledMapTileLayer.Cell());
         Player player = new Player(32, 63, wallLayer, doorLayer);
+
         assertTrue(Collision.collisionCheck(player));
     }
 
@@ -46,6 +48,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(1, 1, new TiledMapTileLayer.Cell());
         Player player = new Player(32, 64, wallLayer, doorLayer);
+
         assertFalse(Collision.collisionCheck(player));
     }
 
@@ -55,6 +58,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(2, 1, new TiledMapTileLayer.Cell());
         Player player = new Player(64 - playerWidth, 32, wallLayer, doorLayer);
+
         assertTrue(Collision.collisionCheck(player));
     }
 
@@ -64,6 +68,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(1, 2, new TiledMapTileLayer.Cell());
         Player player = new Player(32, 63 - playerHeight, wallLayer, doorLayer);
+
         assertFalse(Collision.collisionCheck(player));
     }
 
@@ -73,6 +78,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(1, 2, new TiledMapTileLayer.Cell());
         Player player = new Player(32, 64 - playerHeight, wallLayer, doorLayer);
+
         assertTrue(Collision.collisionCheck(player));
     }
 
@@ -82,6 +88,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(0, 1, new TiledMapTileLayer.Cell());
         Player player = new Player(31, 32, wallLayer, doorLayer);
+
         assertTrue(Collision.collisionCheck(player));
     }
 
@@ -91,6 +98,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(0, 1, new TiledMapTileLayer.Cell());
         Player player = new Player(32, 0, wallLayer, doorLayer);
+
         assertFalse(Collision.collisionCheck(player));
     }
 
@@ -100,6 +108,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(2, 2, new TiledMapTileLayer.Cell());
         Player player = new Player(64 - playerWidth, 64 - playerHeight, wallLayer, doorLayer);
+
         assertTrue(Collision.collisionCheck(player));
     }
 
@@ -109,6 +118,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         doorLayer.setCell(1, 0, new TiledMapTileLayer.Cell());
         Player player = new Player(32, 31, wallLayer, doorLayer);
+
         assertFalse(Player.open);
         assertTrue(Collision.door(player));
         assertTrue(Player.doorInfront);
@@ -120,6 +130,7 @@ public class CollisionTest {
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         doorLayer.setCell(1, 0, new TiledMapTileLayer.Cell());
         Player player = new Player(32, 31, wallLayer, doorLayer);
+
         Player.open = true;
         assertFalse(Collision.door(player));
         assertNull(doorLayer.getCell(1, 1));
