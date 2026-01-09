84741fac2eb48bb1476c1746d2c8dc12b5da7fbd
alistudentgit <dfn510@york.ac.uk>
2026-01-09 05:14:07 +0300

    Update CollisionTest.java

--- core/src/test/java/com/badlogic/debugthugs/CollisionTest.java
+++ core/src/test/java/com/badlogic/debugthugs/CollisionTest.java
@@ -5,47 +5,34 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
 /**
  * Tests the collision system.
- * 
  * Checks the collisions work correctly for all directions in edge cases.
- * This includes checking diagonally. 
- * 
- * Also checks door collisions and that they are disabled when the door is open.
  */
 public class CollisionTest {
 
-    private final float PLAYER_WIDTH = 16;
-    private final float PLAYER_HEIGHT = 16;
+    private final float playerWidth = 16;
+    private final float playerHeight = 16;
 
     @BeforeEach
-    void setUp()
-    {
+    void setUp() {
         // Ensure static variables (in Player) are reset to their initial state.
         Player.doorInfront = false;
         Player.open = false;
     }
 
-    /**
-     * Checks that there will be no collision when no walls (or doors) are present.
-     */
     @Test
-    void noWallMeansNoCollision(){
+    void noWallMeansNoCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         Player player = new Player(32, 32, wallLayer, doorLayer);
         assertFalse(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that the player will collide with a wall which overlaps slightly on the bottom edge.
-     */
     @Test
-    void wallUnderPlayerCollision()
-    {
+    void wallUnderPlayerCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(1, 1, new TiledMapTileLayer.Cell());
@@ -53,12 +40,8 @@ public class CollisionTest {
         assertTrue(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that the player will not collide with a wall even if it is very close to the right side of the player.
-     */
     @Test
-    void wallUnderPlayerNoCollision()
-    {
+    void wallUnderPlayerNoCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(1, 1, new TiledMapTileLayer.Cell());
@@ -66,51 +49,35 @@ public class CollisionTest {
         assertFalse(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that the player will collide with a wall on the right.
-     */
     @Test
-    void wallRightPlayerCollision()
-    {
+    void wallRightPlayerCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(2, 1, new TiledMapTileLayer.Cell());
-        Player player = new Player(64 - PLAYER_WIDTH, 32, wallLayer, doorLayer);
+        Player player = new Player(64 - playerWidth, 32, wallLayer, doorLayer);
         assertTrue(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that the player will not collide with a close wall above.
-     */
     @Test
-    void wallAbovePlayerNoCollision()
-    {
+    void wallAbovePlayerNoCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(1, 2, new TiledMapTileLayer.Cell());
-        Player player = new Player(32, 63 - PLAYER_HEIGHT, wallLayer, doorLayer);
+        Player player = new Player(32, 63 - playerHeight, wallLayer, doorLayer);
         assertFalse(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that the player will collide with a wall above.
-     */
     @Test
-    void wallAbovePlayerCollision()
-    {
+    void wallAbovePlayerCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(1, 2, new TiledMapTileLayer.Cell());
-        Player player = new Player(32, 64 - PLAYER_HEIGHT, wallLayer, doorLayer);
+        Player player = new Player(32, 64 - playerHeight, wallLayer, doorLayer);
         assertTrue(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that the player will collide with a wall on the left.
-     */
     @Test
-    void wallLeftPlayerCollision()
-    {
+    void wallLeftPlayerCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(0, 1, new TiledMapTileLayer.Cell());
@@ -118,12 +85,8 @@ public class CollisionTest {
         assertTrue(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that the player will not collide with a close wall to the left.
-     */
     @Test
-    void wallLeftPlayerNoCollision()
-    {
+    void wallLeftPlayerNoCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(0, 1, new TiledMapTileLayer.Cell());
@@ -131,24 +94,17 @@ public class CollisionTest {
         assertFalse(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that collisions still work on corners.
-     */
     @Test
     void diagonalCornerCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         wallLayer.setCell(2, 2, new TiledMapTileLayer.Cell());
-        Player player = new Player(64-PLAYER_WIDTH, 64-PLAYER_HEIGHT, wallLayer, doorLayer);
+        Player player = new Player(64 - playerWidth, 64 - playerHeight, wallLayer, doorLayer);
         assertTrue(Collision.collisionCheck(player));
     }
 
-    /**
-     * Checks that the player can collide with the door (assuming that the door is closed).
-     */
     @Test
-    void doorCollision()
-    {
+    void doorCollision() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         doorLayer.setCell(1, 0, new TiledMapTileLayer.Cell());
@@ -158,18 +114,13 @@ public class CollisionTest {
         assertTrue(Player.doorInfront);
     }
 
-    /**
-     * Checks that the player can collide with the door (assuming that the door is closed).
-     */
     @Test
-    void doorRemovedWhenOpened()
-    {
+    void doorRemovedWhenOpened() {
         TiledMapTileLayer wallLayer = new TiledMapTileLayer(5, 5, 32, 32);
         TiledMapTileLayer doorLayer = new TiledMapTileLayer(5, 5, 32, 32);
         doorLayer.setCell(1, 0, new TiledMapTileLayer.Cell());
         Player player = new Player(32, 31, wallLayer, doorLayer);
         Player.open = true;
-
         assertFalse(Collision.door(player));
         assertNull(doorLayer.getCell(1, 1));
     }
